66c883790ef847ce0cfa890ad89208fc6109ac1a
Ichisada Shioko <ichisadashioko@gmail.com>
2019-11-18 06:44:05 +0700

    work in progress
    - Next: bs.java

---
+++ src/java/default/package/ClassBS.java
@@ -0,0 +1,14 @@
+package default.package;
+
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+
+public final class ClassBS implements Runnable { // bs
+    public static final int[] INT_ARRAY = new int[]{8, 10, 14, 18}; // a
+    private Display display; // a
+    private ClassR rInstance; // a
+    public static ClassAS asInstance // a
+    private int intC; // c
+    private int intD; // d
+    public int INT_A; // a
+}
